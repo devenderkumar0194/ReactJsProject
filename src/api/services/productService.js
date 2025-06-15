9db3bdcdeab3dcb80ad04fb7399b25383756e2e6
Devender <devenderkumar0194@gmail.com>
2025-06-15 19:33:15 +0530

    update

---
+++ src/api/services/productService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+const backEndURL = "http://localhost:3000";
+
+const getProductsAPI = async () => {
+
+    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NGU2OGRhZDljMjQwOTAzMjFlM2JmMyIsImVtYWlsIjoiZGV2QGdtYWlsLmNvbSIsImlhdCI6MTc0OTk2OTk4NCwiZXhwIjoxNzUwMDU2Mzg0fQ.BbEMAsmtZ1FU-q0QyLfcFgHzTeyDBOWF-QVPyAVPlKo"; // or get from Redux, context, etc.
+    const response = await axios.get(backEndURL+'/products', {
+      headers: {
+        // Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.data;
+}
+
+const productService = {
+    getProductsAPI
+}
+
+export default productService;
--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProductCard from './ProductCard';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../../features/products/productSlice';
 const ProductList = () => {
+    
+    const dispatch = useDispatch();
+    const { items, status, error } = useSelector((state) => state.products);
+    
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, [dispatch]);
+
+    // if (status === 'loading') return <p>Loading...</p>;
+    // if (status === 'failed') return <p>Error: {error}</p>;
+
     return (
     <div className="justify-center min-h-screen bg-blue-100">
         <h1  className="text-4xl mt-1 font-bold  text-blue-800">Product List</h1>    
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4 px-1.5">
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+           {items.map((product, index) =>
+                    <ProductCard key={index} product={product}/>
+            )}
         </div>
     </div>
       
